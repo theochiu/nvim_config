4a036c9f95e673e5d526a612e3b7987778f4d00e
Theodore Chiu <theochiu.me@gmail.com>
2025-07-30 23:33:47 -0400

    removed font, fixed extra ui line

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 -- require("plugins.extras.ui.colorschemes.monokai-pro")
-vim.cmd([[colorscheme carbonfox]])
 
 vim.opt.number = true
 vim.opt.relativenumber = false
