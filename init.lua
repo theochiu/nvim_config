278a2e51a1515526b270a7549e6a8cfb53f78ae5
Theodore Chiu <theochiu.me@gmail.com>
2026-03-25 16:17:44 -0400

    Add keymap to toggle autocompletion in buffer

--- init.lua
+++ init.lua
@@ -7,3 +7,14 @@ vim.opt.relativenumber = false
 -- vim.cmd([[colorscheme carbonfox]])
 vim.cmd([[colorscheme catppuccin-mocha]])
 vim.cmd([[ set nornu]])
+-- toggle on off
+vim.keymap.set("n", "<leader>a", function()
+  -- Toggle the buffer-local variable (defaults to true if nil)
+  if vim.b.completion == nil then
+    vim.b.completion = true
+  end
+  vim.b.completion = not vim.b.completion
+
+  -- Optional: Print a message so you know it worked
+  print("Autocompletion: " .. (vim.b.completion and "On" or "Off"))
+end, { desc = "Toggle autocompletion for current buffer" })
